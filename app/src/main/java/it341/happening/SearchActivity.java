29282e34fa75a1cb966e17c89a1acfaca6cba2a1
Guzmop <gruntworkgames@gmail.com>
2017-03-01 17:04:53 -0600

    added google maps API key

--- app/src/main/java/it341/happening/SearchActivity.java
+++ app/src/main/java/it341/happening/SearchActivity.java
@@ -1,13 +1,27 @@
 package it341.happening;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SearchActivity extends AppCompatActivity {
+import android.util.Log;
+import android.support.v4.app.FragmentActivity;
+
+public class SearchActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // get map fragment and call async to start getting updates
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap map) {
+        Log.d("DEBUG", "onMapReady");
+        map.addMarker(new MarkerOptions().position(new LatLng(40.5123, 88.9947)).title("ISU"));
     }
 }
