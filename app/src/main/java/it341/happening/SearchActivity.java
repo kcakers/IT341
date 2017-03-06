00e25a4021492e9e99b5bc09c57fe52361d86c7f
Guzmop <gruntworkgames@gmail.com>
2017-03-06 16:05:33 -0600

    added some GPS code

--- app/src/main/java/it341/happening/SearchActivity.java
+++ app/src/main/java/it341/happening/SearchActivity.java
@@ -1,10 +1,23 @@
 package it341.happening;
 
 // android
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.Toast;
 
 // maps
 import com.google.android.gms.maps.*;
@@ -15,6 +28,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.yelp.clientlib.connection.YelpAPI;
 import com.yelp.clientlib.connection.YelpAPIFactory;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class SearchActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -28,8 +44,11 @@ public class SearchActivity extends FragmentActivity implements OnMapReadyCallba
     }
 
     GoogleMap map = null;
-    YelpAPIFactory apiFactory = null;
-    YelpAPI yelpAPI = null;
+    YelpAPI yelp = null;
+
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +59,13 @@ public class SearchActivity extends FragmentActivity implements OnMapReadyCallba
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        // init yelp
-        apiFactory = new YelpAPIFactory(
+        // create the yelp api
+        YelpAPIFactory yelpFactory = new YelpAPIFactory(
                 getString(R.string.consumerKey),
                 getString(R.string.consumerSecret),
                 getString(R.string.token),
                 getString(R.string.tokenSecret));
-        yelpAPI = apiFactory.createAPI();
+        yelp = yelpFactory.createAPI();
     }
 
     public void onMapReady(GoogleMap map) {
@@ -57,7 +76,7 @@ public class SearchActivity extends FragmentActivity implements OnMapReadyCallba
     }
 
     public void home(View view) {
-        if(map == null) return;
+        if (map == null) return;
 
         // ISU position and zoom level
         LatLng ISU = new LatLng(40.5123, -88.9947);
@@ -70,5 +89,65 @@ public class SearchActivity extends FragmentActivity implements OnMapReadyCallba
         // apply
         map.addMarker(ISUMarker);
         map.animateCamera(camLocation);
+    }
+
+    public void searchNearMe(View view) {
+
+
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+
+                Log.d("DEBBUG","requestPermissions");
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET}, 10);
+                return;
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+            }
+        } else {
+            Log.d("DEBBUG","else requestLocationUpdates");
+            locationManager.requestLocationUpdates("gps", 5000, 3, locationListener);
+        }
+
+
+    }
+
+    public void onRequestResult(int requestCode, String[] permissions, int[] grantResults) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.d("DEBUG","onRequestPermissionResult");
     }
 }
