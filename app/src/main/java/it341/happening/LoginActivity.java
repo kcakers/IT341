13f02e4d1050101d4c5d2a7cf938c5072dd7da2d
Guzmop <gruntworkgames@gmail.com>
2017-05-02 19:41:50 -0500

    facebook login

--- app/src/main/java/it341/happening/LoginActivity.java
+++ app/src/main/java/it341/happening/LoginActivity.java
@@ -1,13 +1,60 @@
 package it341.happening;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.google.firebase.auth.*;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d("DEBUG","FACEBOOK LOGIN SUCCESS");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d("DEBUG","FACEBOOK LOGIN CANCEL");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d("DEBUG","FACEBOOK LOGIN ERROR");
+            }
+        });
+
     }
+
 }
